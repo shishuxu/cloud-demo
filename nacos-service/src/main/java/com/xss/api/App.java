11f3a9b0eb65873b12404dbad2b3dea4f7eb779e
xushishu <457683048@qq.com>
2021-11-03 15:17:55 +0800

    Initial commit

---
+++ nacos-service/src/main/java/com/xss/api/App.java
@@ -0,0 +1,13 @@
+package com.xss.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
